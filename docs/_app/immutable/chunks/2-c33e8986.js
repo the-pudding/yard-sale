24ee9b2665b59ed5cb9032f4118a06c835af1e95
alvinschang <alvinschang@gmail.com>
2022-12-14 10:08:57 -0500

    update github pages

---
+++ docs/_app/immutable/chunks/2-c33e8986.js
@@ -0,0 +1,1 @@
+import{default as i}from"../components/pages/_page.svelte-d9ec08dd.js";import"./index-cf16332d.js";import"./Header.svelte_svelte_type_style_lang-cc101672.js";import"./index-c7f1c68e.js";const e=!0;export{i as component,e as server};
--- docs/_app/immutable/chunks/2-db5c21eb.js
+++
@@ -1,1 +0,0 @@
-import{default as i}from"../components/pages/_page.svelte-ddb94bc9.js";import"./index-cf16332d.js";import"./Header.svelte_svelte_type_style_lang-cc101672.js";import"./index-c7f1c68e.js";const e=!0;export{i as component,e as server};
